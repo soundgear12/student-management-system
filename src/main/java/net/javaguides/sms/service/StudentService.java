3fc5550d2ddd066744de12551958d33702c618fb
Antonio <antonio.rodriguez024@gmail.com>
2021-10-06 00:44:49 -0500

    initial commit

---
+++ src/main/java/net/javaguides/sms/service/StudentService.java
@@ -0,0 +1,10 @@
+package net.javaguides.sms.service;
+
+import java.util.List;
+
+import net.javaguides.sms.entity.Student;
+
+public interface StudentService {
+
+	List<Student> getAllStudents();
+}
